32b7bc76dfafc50b23922b6e9460e4da82c64861
anuragsayshelloworld <anuragsayshelloworld@gmail.com>
2025-07-13 14:11:12 +0545

    Task Completion: Added AI is typing animation and picture for Bot and Users

--- src/components/AIChat/ChatMessages.tsx
+++ src/components/AIChat/ChatMessages.tsx
@@ -6,6 +6,7 @@ import {
   Stack,
   Text,
   Textarea,
+  Skeleton
 } from "@mantine/core";
 import axios from "axios";
 import {
@@ -137,15 +138,40 @@ const ChatMessages = ({ chatId, input, setInput }: MessageProps) => {
         )}
         <ScrollArea className={styles.messageList}>
           {messages.map((message) => (
-            <div
-              key={message.id}
-              className={
-                message.isUser ? styles.userMessage : styles.botMessage
-              }
-            >
-              <Text>{message.content}</Text>
-            </div>
+
+        <div
+            key={message.id}
+            className={`${styles.messageContainer} ${
+            message.isUser ? styles.userMessageContainer : styles.botMessageContainer}`}>
+       
+       
+        <img 
+           src={message.isUser ? '/user.png' : '/probot.png'} 
+           className={styles.avatarImage}/>
+
+        <div className={`${styles.messageContent} ${
+            message.isUser ? styles.userMessage : styles.botMessage}`}>
+    
+        <Text>{message.content}</Text>
+        </div>
+        </div>
           ))}
+         
+
+          {isLoading && (
+            <Group gap="xs">
+              <Text size="sm" c="dimmed">AI is ruminating...</Text>
+                  <Group gap={4}>
+                      <Skeleton height={8} width={8} radius="xl" />
+                      <Skeleton height={8} width={8} radius="xl" />
+                      <Skeleton height={8} width={8} radius="xl" />
+                   </Group>
+                   </Group>
+                   )}
+
+
+        {/*Chage made on 7/13/2025 by AIA*/}
+
         </ScrollArea>
       </Box>
       <form onSubmit={handleSubmit} className={styles.inputForm}>
