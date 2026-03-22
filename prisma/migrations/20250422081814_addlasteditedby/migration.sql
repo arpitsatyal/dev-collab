30d35bf6fb763096469291a3beb712fe923a7311
arpit <arpited7@gmail.com>
2026-03-22 23:05:14 +0545

    Remove all api files

--- prisma/migrations/20250422081814_addlasteditedby/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "Snippet" ADD COLUMN     "lastEditedById" TEXT;
-
--- AddForeignKey
-ALTER TABLE "Snippet" ADD CONSTRAINT "Snippet_lastEditedById_fkey" FOREIGN KEY ("lastEditedById") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
